9cdd9367eaeb1b1190301dff2222d38742ad2678
AsureDay <73357830+AsureDay@users.noreply.github.com>
2020-11-12 01:22:35 +0700

    copy pasted some things
    renaming and creating tamplates later

---
+++ binary_tree/main.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_RUNNER
+#include "../catch.hpp"
+#include "binary_tree.h"
+#include "test_time.h"
+
+int main(int argc, char** argv) {
+
+	testTime(100000,1000,100);
+	
+	return 0;
+}
