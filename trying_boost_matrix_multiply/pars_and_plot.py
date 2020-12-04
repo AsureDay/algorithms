de3b1747baf6f862f6930e3d4047fc9299bdb029
AsureDay <73357830+AsureDay@users.noreply.github.com>
2020-12-04 08:40:39 +0700

    calculated data

--- trying_boost_matrix_multiply/pars_and_plot.py
+++ trying_boost_matrix_multiply/pars_and_plot.py
@@ -4,6 +4,8 @@ classic_method_time_list = []
 better_method_time_list = []
 nlist = []
 
+fig, ax = plt.subplots()
+
 float_num_pattern = r"[-+]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][-+]?\d+)?"
 int_num_pattern = r"[-+]?\d+"
 with open("tests_withO.txt",'r') as file:
@@ -15,6 +17,8 @@ with open("tests_withO.txt",'r') as file:
 		elif(line[0] == 'b'):
 			better_method_time_list.append(float(re.search(float_num_pattern,line)[0]))
 
+ax.set_xlabel("Размер матрицы (N)")
+ax.set_ylabel("Время перемножения (с)")
 print(nlist)
 #plt.plot(nlist,classic_method_time_list)
 plt.plot(nlist,better_method_time_list)
